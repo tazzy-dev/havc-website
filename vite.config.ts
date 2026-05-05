bef7701892d2b3a496256720e7b4be57a853983b
Tayyab Waseem <thisistayyab089@gmail.com>
2026-05-05 19:59:26 +0500

    Fix: path aliasing and build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/havc-website/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
